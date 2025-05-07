4ac2806b4f5caba35d3deeba87ac9e6817b91244
PhilTech-pixel <njoromanga49@gmail.com>
2025-05-07 19:00:16 +0300

    Map Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
+import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
-import PlainInfo from "./plaininfo";
+
+const INITIAL_CENTER = [-74.0242, 40.6941];
+const INITIAL_ZOOM = 10.12;
 
 function App() {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  const [center, setCenter] = useState(INITIAL_CENTER);
+  const [zoom, setZoom] = useState(INITIAL_ZOOM);
+
+  useEffect(() => {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoicHdubGFiIiwiYSI6ImNtYWUzaXV4NTAzZDQya3Nnendqc3lyZTYifQ.X7ux09Uxtzp7h5dhQGrEUA";
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: center,
+      zoom: zoom,
+    });
+
+    mapRef.current.on("move", () => {
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter();
+      const mapZoom = mapRef.current.getZoom();
+
+      // update state
+      setCenter([mapCenter.lng, mapCenter.lat]);
+      setZoom(mapZoom);
+    });
+
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+
   return (
     <>
-      <h1>Flight Tracker App</h1>
-      <h1>{PlainInfo}</h1>
+      <div className="sidebar">
+        <h2>Flight Tracker App</h2>
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
+      </div>
+      <div id="map-container" ref={mapContainerRef} />
     </>
   );
 }
