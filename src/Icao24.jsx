159c85d3788ecd2b282a14b967388e17ea178db0
PhilTech-pixel <njoromanga49@gmail.com>
2025-05-10 19:58:19 +0300

    Render JSON Flight Data from ICAO24

--- src/Icao24.jsx
+++ src/Icao24.jsx
@@ -4,6 +4,7 @@ function Icao24() {
   const [IcaoNo, setIcaoNo] = useState("");
 
   // User search Data using plane ICAO Number
+  const [data, setData] = useState([]);
 
   const fetchFlightData = async () => {
     try {
@@ -11,12 +12,13 @@ function Icao24() {
         `https://opensky-network.org/api/flights/aircraft?icao24=${IcaoNo}&begin=1517184000&end=1517270400`
       );
 
-      const data = await response.json();
+      setData(await response.json());
       console.log(data); // Log the data or handle it as needed
     } catch (error) {
       console.error("Error fetching flight data:", error); // Handle errors
     }
   };
+  const key = Math.floor(Math.random() * 1000);
 
   return (
     <div className="icao24">
@@ -29,10 +31,15 @@ function Icao24() {
         placeholder="Enter ICAO24 Number"
       />
       <button onClick={fetchFlightData}>Search</button>
+      <div className="icao24-data">
+        <ol>
+          {data.map((flightdata) => {
+            return <li>{flightdata.callsign}</li>;
+          })}
+        </ol>
+      </div>
     </div>
   );
-
-  // Get data from API
 
   // Render the data in the map (maybe in a sidebar)
 }
